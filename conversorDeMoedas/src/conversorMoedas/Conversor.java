3c8fd8cc0272b5ed2a0e3990f7b7ec0946942d61
Danilo Acioly <144499500+daniloacioly@users.noreply.github.com>
2024-10-10 10:22:44 -0300

    Add files via upload

---
+++ conversorDeMoedas/src/conversorMoedas/Conversor.java
@@ -0,0 +1,48 @@
+package conversorMoedas;
+
+import javax.swing.*;
+
+public class Conversor {
+    public static void main(String[] args) {
+        Funcao moedas = new Funcao();
+        
+
+        while (true) {
+
+            String opcao = JOptionPane.showInputDialog(null, "Escolha uma opção ", "Menu", JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Conversor de Moeda", "Conversor de Temperatura"}, "Escolha").toString();
+
+            switch (opcao) {
+                case "Conversor de Moeda":
+                    String input = JOptionPane.showInputDialog("Insira um valor: ");
+                    if (checar(input)) {
+                        double valorRecebido = Double.parseDouble(input);
+                        moedas.converterMoeda(valorRecebido);
+
+                        int resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+                        if (JOptionPane.OK_OPTION == resposta) {
+                            System.out.println("Escolha opção Afirmativa");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Valor inválido");
+                    }
+                    break;
+                case "Conversor de Temperatura":
+                	JOptionPane.showMessageDialog(null, "Em Construção!");
+                	
+                    }
+                    break;
+            }
+        }
+
+    public static boolean checar(String input) {
+        try {
+            double x = Double.parseDouble(input);
+            if (x >= 0 || x < 0) ;
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
